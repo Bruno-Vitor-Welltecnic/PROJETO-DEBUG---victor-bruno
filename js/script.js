ccaddce80c6604230f8cbacf43e0db4414194606
Logon Aluno <brunobiletsky@gmail.com>
2024-05-23 10:08:03 -0300

    fim de aula

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ const btnMobile = document.querySelector("#btnMobile");
 //FUNÇÃO MENU
 function Menu(){
     //DECLARANDO A VARIAVEL E PEGANDO O  ID
-   const navBar = document.querySelector(".nav");
+   const navBar = document.querySelector("nav");
    //RETORNA UMA LISTA ATIVA
     navBar.classList.toggle("active");
 }
 //EVENTO QUE AO CLICAR CHAMA FUNÇÃO MENU
-btnMobile.addEventListener("clique", Menu);
+btnMobile.addEventListener("click", Menu);
 
 
 /*criando o slideShow */
 
 //declarando o objeto de imagens em um array
-let imagens = ['img/carro1', 'img/Carro2.png', 'img/Carro3.png'];
+let imagens = ['/img/carro1', '/img/Carro2.png', '/img/Carro3.png'];
 //declarando a posição de inicio
 let Index = 0;
 //declarando o tempo que as imagens vão passar em milesegundos
@@ -28,18 +28,18 @@ function slideShow() {
   //pegando o id da imagem no html para poder carregar as imagens usando DOM
   document.getElementById('img').src = imagens[Index];
   //incremento das imagens e sua posição
-  Index++;
+  Index = Index + 1;
 
   //se a posição da imagem for imagem e a quantidade de imagens no array
 
-  if (Index == imagens.lenght) {
+  if (Index == imagens.length) {
     Index = 0;
   }
   //setTimeout é uma função que executa o tempo, nesse caso vai chamar função e aplicar o time
-  setTimeout('slideshow()', time);
+  setTimeout(slideShow(), time);
 }
 //executar toda a função
-slideshow();
+slideShow();
 
 
 
